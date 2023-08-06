df84f53b5e351dade3f4757a532d1a1352217033
ShevkoplyasDarya <shevkoplyund@gmail.com>
2023-08-06 14:48:20 +0200

    fixed search in Pages, added check for code example

--- src/test/java/guthub/SelenideWikiTest.java
+++ src/test/java/guthub/SelenideWikiTest.java
@@ -1,6 +1,8 @@
 package guthub;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -17,19 +19,36 @@ public class SelenideWikiTest {
         Configuration.pageLoadStrategy = "eager";
     }
 
+    @AfterAll
+    static void afterAll() {
+        Selenide.closeWebDriver();
+    }
+
 
     @Test
-    void shouldFindSelenideWikiOnGithub() {
+    void shouldFindSoftAssertions() {
         //open selenide page
         open("https://github.com/selenide/selenide");
         //open Wiki
         $("#wiki-tab").click();
-        // check if page Soft assertions is present
-        $("#wiki-body").shouldHave(text("Soft assertions"));
-        //open page SoftAssertions
-        $("#wiki-body").$(byText("Soft assertions")).click();
+        //search for SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        //go to SoftAssertions page
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         //find code for JUnit5
-        $("#wiki-body").shouldHave(text("JUnit5"), text("@ExtendWith({SoftAssertsExtension.class})"));
+        $(".markdown-body").shouldHave(text(
+                "3. Using JUnit5 extend test class:\n" +
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
     }
 
 }
